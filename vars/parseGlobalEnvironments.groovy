fbc004642caf383b07b377b34a9b02b02eb28df5
Shashi ebbare <syebbare@skydevops.in>
2020-02-09 08:28:18 +0530

    just for parsing env

--- vars/parseGlobalEnvironments.groovy
+++ vars/parseGlobalEnvironments.groovy
@@ -4,18 +4,28 @@ def call(){
     stages {
       stage('Stage Environments') {
         steps {
-          sh '''
-          if [ ! -d "/var/lib/jenkins/chef_automation/global_envs" ]; then
-            mkdir -p /var/lib/jenkins/chef_automation/global_envs
-          fi
-          cp * /var/lib/jenkins/chef_automation/global_envs/
-          '''
+          if(isUnix()){
+            sh '''
+            if [ ! -d "/var/lib/jenkins/chef_automation/global_envs" ]; then
+              mkdir -p /var/lib/jenkins/chef_automation/global_envs
+            fi
+            cp * /var/lib/jenkins/chef_automation/global_envs/
+            '''
+          }else{
+            powershell '''
+              if (!(Test-Path -Path "D:\var\lib\jenkins\chef_automation\global_envs")) {
+                    mkdir -p "D:\var\lib\jenkins\chef_automation\global_envs"
+                }
+            '''
+          }
         }
       }
       stage('Publish Environments to Production') {
         steps {
           input 'Publish Enviornments to Production Chef Server?'
-          sh 'chef exec ruby /var/lib/jenkins/chef_automation/update_global_env_pins.rb -k /var/lib/jenkins/chef_repo/.chef/knife.rb -f /var/lib/jenkins/chef_automation'
+          powershell ''' 
+          chef exec ruby D:\var\lib\jenkins\chef_automation\update_global_env_pins.rb -k D:\var\lib\jenkins\chef_repo\.chef\knife.rb -f D:\var\lib\jenkins\chef_automation
+          '''
         }
       }
     }
